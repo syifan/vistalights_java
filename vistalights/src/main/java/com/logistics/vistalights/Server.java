980b09548ca8e7a0809ca241b966490bcdb20609
Yifan Sun <sunyifan112358@gmail.com>
2015-12-27 22:18:26 -0500

    Init commit

---
+++ vistalights/src/main/java/com/logistics/vistalights/Server.java
@@ -0,0 +1,16 @@
+package com.logistics.vistalights;
+
+import com.logistics.pvis.frame.Frame;
+import com.logistics.pvis.frame.ProcessingFrame;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Frame frame = new ProcessingFrame();
+		frame.setWidth(960);
+		frame.setHeight(480);
+		frame.show();
+
+	}
+
+}
